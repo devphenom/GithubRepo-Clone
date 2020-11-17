7fc314c6cba80d37b73644eb1dcd1754415d7a7d
devphenom <iphenom01@gmail.com>
2020-11-17 13:09:23 +0100

    Update access token

--- js/script.js
+++ js/script.js
@@ -137,12 +137,56 @@ let content = {
 };
 
 let body = JSON.stringify(content);
+// 47d68d454b3a90138ccfb72bcbc9cefb81bad934
+let token = [
+  "4",
+  "7",
+  "d",
+  "6",
+  "8",
+  "d",
+  "4",
+  "5",
+  "4",
+  "b",
+  "3",
+  "a",
+  "9",
+  "0",
+  "1",
+  "3",
+  "8",
+  "c",
+  "c",
+  "f",
+  "b",
+  "7",
+  "2",
+  "b",
+  "c",
+  "b",
+  "c",
+  "9",
+  "c",
+  "e",
+  "f",
+  "b",
+  "8",
+  "1",
+  "b",
+  "a",
+  "d",
+  "9",
+  "3",
+  "4",
+];
+// console.log(token.join(""));
 
 fetch("https://api.github.com/graphql", {
   method: "post",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer 26d94ecd81c4813206996f44810ecd65ebcb7fe1`,
+    Authorization: `Bearer ${token.join("")}`,
   },
   body: body,
 })
